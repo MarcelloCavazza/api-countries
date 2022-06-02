542013ba8315722f503c5092cc9c59da6ba9bb5e
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-06-02 11:09:38 -0300

    removido console logs

--- js/main.js
+++ js/main.js
@@ -118,9 +118,7 @@ async function doFetchAsync() {
     })
 
     btnClearSearchField.addEventListener("click", () => {
-        console.log(searchField.value)
         searchField.value = ""
-        console.log(searchField.value)
         continentIsChecked.checked = false
         let populationCount = 0;
         for (i = 0; i < apiData.length; i++) {
